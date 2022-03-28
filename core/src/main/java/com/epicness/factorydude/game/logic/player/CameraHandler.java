a96479637429668a0ef09187d726ee79919cf1ff
Luis Frontanilla <luis.frontanilla@fundacion-jala.org>
2022-03-27 22:41:29 -0400

    Camera constrained.

--- core/src/main/java/com/epicness/factorydude/game/logic/player/CameraHandler.java
+++ core/src/main/java/com/epicness/factorydude/game/logic/player/CameraHandler.java
@@ -1,5 +1,11 @@
 package com.epicness.factorydude.game.logic.player;
 
+import static com.epicness.factorydude.game.GameConstants.FLOOR_HEIGHT;
+import static com.epicness.factorydude.game.GameConstants.FLOOR_WIDTH;
+import static com.epicness.fundamentals.SharedConstants.CAMERA_HEIGHT;
+import static com.epicness.fundamentals.SharedConstants.CAMERA_WIDTH;
+
+import com.badlogic.gdx.math.MathUtils;
 import com.epicness.factorydude.game.stuff.GameStuff;
 import com.epicness.fundamentals.SharedScreen;
 
@@ -12,7 +18,9 @@ public class CameraHandler {
     public void update() {
         float playerX = stuff.getPlayer().getX();
         float playerY = stuff.getPlayer().getY();
-        screen.getDynamicCamera().position.set(playerX, playerY, 0f);
+        float x = MathUtils.clamp(playerX, CAMERA_WIDTH / 2f, FLOOR_WIDTH - CAMERA_WIDTH / 2f);
+        float y = MathUtils.clamp(playerY, CAMERA_HEIGHT / 2f, FLOOR_HEIGHT - CAMERA_HEIGHT / 2f);
+        screen.getDynamicCamera().position.set(x, y, 0f);
         screen.getDynamicCamera().update();
     }
 
